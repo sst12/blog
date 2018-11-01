7bec7d35bf41bdc430d2dda9aa7e8b33e1967270
sst12 <saurabhtanwar05744@gmail.com>
2018-11-01 18:34:27 +0530

    My Djangopro app, first commit

---
+++ activate.sh
@@ -0,0 +1,1 @@
+source /home/mochis/Documents/script/activate.sh
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mochis/Documents/djangopro/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
